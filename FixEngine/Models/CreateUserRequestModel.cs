4a96fad335712be58f309dc5d905a2c6e8124a0d
AbdullahMo5 <abdallah@finitic.com>
2024-10-28 12:18:20 +0300

    add a validtion message for password

--- FixEngine/Models/CreateUserRequestModel.cs
+++ FixEngine/Models/CreateUserRequestModel.cs
@@ -10,6 +10,7 @@ namespace FixEngine.Models
         public string FirstName { get; set; }
         [MaxLength(250)]
         public string LastName { get; set; }
+        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_]).{8,}$", ErrorMessage = "Password must be at least 8 characters long and includes at least one uppercase letter, one lowercase letter, one digit, and one special character (e.g., !, @, #, $).")]
         public string Password { get; set; }
         [Compare("Password")]
         public string ConfirmPassword { get; set; }
